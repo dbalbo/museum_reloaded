f0cd06778c73a5919b8af4f25ca1c8e87094e74b
Guest User <student@epicodus.com>
2015-06-16 16:50:25 -0700

    allroutes are working

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ DB = PG.connect({:dbname => "museum_tracker"})
 
 get('/') do
 	@museums = Museum.all
-erb(:index)	
+erb(:index)
 end
 
 post('/') do
@@ -29,12 +29,12 @@ get('/museums/:id') do
 	erb(:museum)
 end
 
-post('/artworks/"id') do
+post('/museums/:id') do
 	@museum  = Museum.find(params.fetch('id').to_i)
+	x = @museum.id
 	description = params.fetch('description')
-	@artwork = Artwork.new({:description => description, :museum_id => museum_id, :id =>nil})
+	@artwork = Artwork.new({:description => description, :museum_id => x, :id =>nil})
 	@artwork.save
-	@museum = Museum.find(museum_id)
 	redirect back
 end
 
@@ -52,3 +52,18 @@ delete('/') do
   erb(:index)
 end
 
+get('/artworks/:id') do
+	@artwork = Artwork.find(params.fetch('id').to_i)
+	@museum = Museum.find(@artwork.museum_id)
+	erb(:artwork)
+end
+
+delete('/museums/:id') do
+	x = params.fetch('a_id').to_i
+	@artwork = Artwork.find(x)
+	y = @artwork.museum_id
+	@artwork.delete
+	@museum = Museum.find(y)
+	erb(:museum)
+
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,6 +1,16 @@
 
-# require('capybara/rspec')
-# require('./app')
-# require('spec_helper')
-# Capybara.app == Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+require('spec_helper')
+Capybara.app == Sinatra::Application
+set(:show_exceptions, false)
+
+
+describe('the add a museum path') do
+  it('adds a new museum to the list of museums') do
+    visit('/')
+    fill_in('Add a Museum to the National Database', :with => "The Guggenheim" )
+    click_button("Add Museum")
+    expect(page).to have_content("The Guggenheim")
+  end
+end
